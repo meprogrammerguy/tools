d17599511e80517c57314a96cca95a78ad791055
meprogrammerguy <b65535@gmail.com>
2015-07-29 08:13:54 -0500

    Create GetAutoBuild script

---
+++ ps/GetAutoBuild.ps1
@@ -0,0 +1,18 @@
+<#
+    Powershell GetAutoBuild
+    I prefer to work on my box local. This script will go to all the build servers and obtain the current copy of COMMANDseriesBuild.
+    It will Name them so you know what box they came from. The script then checks out the COMMANDseriesBuild and copies it to my working Folder
+    I will then do Compares of the build server files (manually using beyond compare). update my .bld so it is current. make my changes.
+    ==
+    I will the Create 2 more Powershell scripts (TestAutoBuild, and PutAutoBuild)
+#>
+New-Item -Force -ItemType directory -Path C:\Users\jsmith\Desktop\AutoBuild
+Copy-Item \\al-csbuild-s04\build\COMMANDseries\COMMANDseriesBuild.bld C:\Users\jsmith\Desktop\AutoBuild\COMMANDseriesBuild.s04
+Copy-Item \\al-csbuild-s05\build\COMMANDseries\COMMANDseriesBuild.bld C:\Users\jsmith\Desktop\AutoBuild\COMMANDseriesBuild.s05
+Copy-Item \\al-csbuild-vm1\build\COMMANDseries\COMMANDseriesBuild.bld C:\Users\jsmith\Desktop\AutoBuild\COMMANDseriesBuild.vm1
+h:
+cd H:\unicomp\AutoBuild\COMMANDseries
+$tf = &"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\tf.exe" checkout $/AutoBuild/COMMANDseries/COMMANDseriesBuild.bld /noprompt
+$tf | Out-null
+c:
+Copy-Item H:\unicomp\AutoBuild\COMMANDseries\COMMANDseriesBuild.bld C:\Users\jsmith\Desktop\AutoBuild\COMMANDseriesBuild.bld
