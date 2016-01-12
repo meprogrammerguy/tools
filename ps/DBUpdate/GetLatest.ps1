d7828de0c55ea5dd3858c2d94409edc0198f08be
meprogrammerguy <b65535@gmail.com>
2016-01-12 07:21:54 -0600

    Make less hard coded

--- ps/DBUpdate/GetLatest.ps1
+++ ps/DBUpdate/GetLatest.ps1
@@ -1,25 +1,25 @@
 <#
     Powershell DBUpdate
 #>
+$CSVersion = "2.27"
+$pieces = $CSVersion.split(".")
+$CSVersionMajor = $pieces[0]
+$CSVersionMinor = $pieces[1]
+$UnifaceIDFLocation = "T:\UNIFACE\U9605\X505\common\BIN\idf.exe"
+$CSMessageLocation = "H:\unicomp\CSCE\CS06\CS08." + $CSVersion + "\USYS"
+$CSComponentLocation = "H:\unicomp\CSCE\CS06\CS08." + $CSVersion + "\Components"
+$CSModelLocation = "H:\unicomp\CSCE\CS06\CS08." + $CSVersion + "\Models"
+$TFSLocation = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\tf.exe"
+$MainLocation = "P:\CS08_2X\CS08_" + $CSVersionMajor + "_" + $CSVersionMinor + "\USERS\MAIN\"
 $PatternStart = '<DAT name=\"ULABEL\">'
 $PatternEnd = "</DAT>"
-$Patterns = @()
-$Tables = Read-Host -Prompt 'Table(s) ordr,schl(<cr>=all)'
-$Pieces = $Tables.split(",")
-if ($Pieces[1] -gt "")
-{
-	foreach ($Piece in $Pieces)
-	{
-		$Piece = $Piece.ToUpper()
-		$Patterns = $Patterns + ($PatternStart + $Piece + $PatternEnd)
-	}
-}
-$Patterns.Count
-if ($Patterns.Count -le 0)
-{
-	"None";
-}
-else
-{
-	$Patterns
-}
+
+$CSVersion 
+$CSVersionMajor
+$CSVersionMinor
+$UnifaceIDFLocation 
+$CSMessageLocation 
+$CSComponentLocation
+$CSModelLocation
+$TFSLocation
+$MainLocation
