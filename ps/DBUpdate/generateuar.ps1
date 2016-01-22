8e54b559c91213d3c6afddcc8885bff54a87574e
meprogrammerguy <b65535@gmail.com>
2016-01-22 13:10:56 -0600

    Just generate messages

--- ps/DBUpdate/generateuar.ps1
+++ ps/DBUpdate/generateuar.ps1
@@ -2,48 +2,23 @@
     Powershell DBUpdate
 #>
 clear
-$CSVersion = "CS08.2.27"
 $UnifaceIDFLocation = "T:\UNIFACE\U9605\X505\common\BIN\idf.exe"
-$UnifaceININame = "idf96.ini"
-$pieces = $CSVersion.split(".")
-$CSPrefix = $pieces[0]
-$CSVersionMajor = $pieces[1]
-$CSVersionMinor = $pieces[2]
-$CSParent = "CS06"
-if ($CSVersionMajor -ne "2")
-{
-  $CSParent = "CSPV6"
-}
 $CSMessageLocation = "d:\messages"
+$CDMessageOld = $CSMessageLocation + "\messagesgenerated.old"
+$CDMessageNew =  $CSMessageLocation + "\messagesgenerated.uar"
 $CSMessageArgs = "$/CSCE/CS06/CS08.2.27/USYS/messagesgenerated.uar" 
-$CSComponentLocation = "H:\unicomp\CSCE\" + $CSParent + "\" + $CSVersion + "\Components"
-$CSComponentFiles = $CSComponentLocation + "\*.cmx"
-$CSComponentArgs = "$/CSCE/CS06/CS08.2.27/USYS/Components"
-$CSModelLocation = "H:\unicomp\CSCE\" + $CSParent + "\" + $CSVersion + "\Models"
+$CSModelLocation = "H:\unicomp\CSCE\CS06\CS08.2.27\Models"
 $CSModelArgs = "$/CSCE/CS06/CS08.2.27/USYS/Models"
 $TFSLocation = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\tf.exe"
-$AsnLocation = "D:\DBUpdate\Devo_v2\"
-$INILocation = "/ini=P:\CS08_2X\CS08_2_27\USERS\MAIN\idf96.ini"
-$ImportComponents = "XML:H:\Unicomp\CSCE\CS06\CS08.2.27\Components\*.cmx"
 $ImportModels = "XML:H:\Unicomp\CSCE\CS06\CS08.2.27\Models\*.xml"
-$PatternStart = '<DAT name=\"ULABEL\">'
-$PatternEnd = "</DAT>"
 $AsnGenerated = "D:\DBUpdate\MessagesGenerated_v2\"
 $URRGenerated = "D:\DBUpdate\MessagesGenerated_v2\messagesgenerated.uar"
 $INIGenerated = "/ini=D:\DBUpdate\MessagesGenerated_v2\idf96.ini"
 $ResourcesGenerated = "D:\DBUpdate\MessagesGenerated_v2\resources\msg"
 $ZipLocation = "D:\DBUpdate\MessagesGenerated_v2\7za.exe"
 $ZipArgs = "a -tzip "
-$DevoLogs = "D:\DBUpdate\Devo_v2\Logs\"
 $GeneratedLogs = "D:\DBUpdate\MessagesGenerated_v2\Logs\"
 $CurrentUser = [Environment]::UserName
-$CSPrompt = $CSVersion + ' DBUpdate script - Table(s) ordr,schl (<cr>=all)'
-$LoadUCDataLocation = $AsnLocation  + "loaducdata.exe"
-$LoadUCDataFile = "P:\CS08_2X\CS08_2_27\UTILS\LoadUCData.sql"
-$LoadUCDataTempFile = $AsnLocation  + "LoadUCData.sql"
-$LoadUCDataOld = $AsnLocation  + "LoadUCData.old"
-$LoadUCDataASN = $AsnLocation + "idf.asn"
-$LoadUCDataArgs = $LoadUCDataASN + "," + $LoadUCDataFile
 function GetElapsedTime([datetime]$starttime) 
 {
     $runtime = $(get-date) - $starttime
@@ -51,25 +26,12 @@ function GetElapsedTime([datetime]$starttime)
     $retStr
 }
 
-$Tables = Read-Host -Prompt $CSPrompt
-
 $script:startTime = Get-Date
 [Environment]::UserName
 [Environment]::UserDomainName
 [Environment]::MachineName
 Add-PSSnapin Microsoft.TeamFoundation.PowerShell
 write-host "Script Started at $script:startTime" -foreground "green"
-
-$Pieces = $Tables.split(",")
-if ($Pieces[0] -gt "")
-{
-  $Patterns = @()
-	foreach ($Piece in $Pieces)
-	{
-		$Piece = $Piece.ToUpper()
-		$Patterns = $Patterns + @($PatternStart + $Piece + $PatternEnd)
-	}
-}
 
 cd $CSMessageLocation
 Convert-Path .
@@ -79,38 +41,30 @@ if ($LockTest -match "no pending")
   $itemtime = Get-Date
   write-host "$(get-date) Checking out the messagesgenerated.uar file" -foreground "green"
   & $TFSLocation get $CSMessageArgs | Out-null
+<#
   & $TFSLocation checkout $CSMessageArgs | Out-null
-  & $TFSLocation status /user:* /format:detailed $CSMessageArgs | Out-null
+#>
+  $LockTest = & $TFSLocation status /user:* /format:detailed $CSMessageArgs | Out-null
 }
 else
 {
   $itemtime = Get-Date
-  write-host "$(get-date) Someone Is Running The Updates Tool - exiting now" -foreground "red"
+  write-host "$(get-date) Someone is currently generating messages - exiting now" -foreground "red"
   $LockTest
   cd $PSScriptRoot
   Convert-Path .
   write-host "Script Ended at $(get-date)" -foreground "red"
   $elapsed = GetElapsedTime $script:startTime
   write-host "Total Elapsed Time: " $elapsed;
-  <#
+<#
   Exit
-  #>
+#>
 }
-
-cd $DevoLogs
-Convert-Path .
-write-host "$(get-date) Removing old log files" -foreground "green"
-foreach ($file in Get-ChildItem -name)
-{
-  if ($file -match $CurrentUser)
-  {
-    Remove-Item $file -force
-  } 
-}
+$LockTest
 
 cd $GeneratedLogs
 Convert-Path .
-write-host "$(get-date) Removing old log files" -foreground "green"
+write-host "$(get-date) Removing old uniface log files" -foreground "green"
 foreach ($file in Get-ChildItem -name)
 {
   if ($file -match $CurrentUser)
@@ -118,14 +72,6 @@ foreach ($file in Get-ChildItem -name)
     Remove-Item $file -force
   } 
 }
-
-cd $CSComponentLocation
-Convert-Path .
-$itemtime = Get-Date
-write-host "$(get-date) Getting Latest Components" -foreground "green"
-& $TFSLocation get $CSComponentArgs | Out-null
-$elapsed = GetElapsedTime $itemtime
-write-host "Elapsed Time: " $elapsed -foreground "green"
 
 cd $CSModelLocation
 Convert-Path .
@@ -134,90 +80,6 @@ write-host "$(get-date) Getting Latest Models" -foreground "green"
 & $TFSLocation get $CSModelArgs | Out-null
 $elapsed = GetElapsedTime $itemtime
 write-host "Elapsed Time: " $elapsed -foreground "green"
-
-cd $AsnLocation 
-Convert-Path .
-$itemtime = Get-Date
-write-host "$(get-date) Importing Components" -foreground "green"
-& $UnifaceIDFLocation $INILocation /imp $ImportComponents | Out-null
-$elapsed = GetElapsedTime $itemtime
-write-host "Elapsed Time: " $elapsed -foreground "green"
-$itemtime = Get-Date
-write-host "$(get-date) Importing Models" -foreground "green"
-& $UnifaceIDFLocation $INILocation /imp $ImportModels | Out-null
-$elapsed = GetElapsedTime $itemtime
-write-host "Elapsed Time: " $elapsed -foreground "green"
-$itemtime = Get-Date
-write-host "$(get-date) Analyizing Models" -foreground "green"
-& $UnifaceIDFLocation $INILocation /con | Out-null
-$elapsed = GetElapsedTime $itemtime 
-write-host "Elapsed Time: " $elapsed -foreground "green"
-$itemtime = Get-Date
-write-host "$(get-date) Generating R, S and Y messages" -foreground "green"
-& $UnifaceIDFLocation $INILocation /tst gen_messages.aps RSY | Out-null
-$elapsed = GetElapsedTime $itemtime
-write-host "Elapsed Time: " $elapsed -foreground "green"
-
-if ($Patterns.Count -le 0)
-{
-  $itemtime = Get-Date
-  write-host "$(get-date) Compiling all Services" -foreground "green"
-	& $UnifaceIDFLocation $INILocation /svc /inf | Out-null
-  $elapsed = GetElapsedTime $itemtime
-  write-host "Elapsed Time: " $elapsed -foreground "green"
-  $itemtime = Get-Date
-  write-host "$(get-date) Compiling all Forms" -foreground "green"
-	& $UnifaceIDFLocation $INILocation /frm /inf | Out-null
-}
-else
-{
-  $itemtime = Get-Date
-	foreach ($file in Get-ChildItem -Path $CSComponentFiles | Select-String -pattern $Patterns | Select-Object -Unique path)
-	{
-		$filespec = $file.path
-		$pieces = $filespec.split("\")
-		$filename = $pieces[$pieces.count - 1]
-		$pieces = $filename.split(".")
-		$justname = $pieces[$pieces.count - 2]
-		write-host "$(get-date) Compiling $justname" -foreground "green"
-		& $UnifaceIDFLocation $INILocation /cpt /inf $justname | Out-null
-	}
-}
-$elapsed = GetElapsedTime $itemtime
-write-host "Elapsed Time: " $elapsed -foreground "green"
-
-If (Test-Path $LoadUCDataOld)
-{
-  write-host "$(get-date) Removing old LoadUCData file" -foreground "green"
-	Remove-Item $LoadUCDataOld -force
-}
-If (Test-Path $LoadUCDataFile)
-{
-  write-host "$(get-date) Renaming LoadUCData.sql to LoadUCData.old" -foreground "green"
-  Rename-Item $LoadUCDataFile LoadUCData.old
-}
-$itemtime = Get-Date
-write-host "$(get-date) Generating LoadUCData.sql (oracle DB only)" -foreground "green"
-$outputTool = & $LoadUCDataLocation ora, $LoadUCDataASN, $LoadUCDataTempFile
-write-output $outputTool
-if ($outputTool -match "error")
-{
-  $elapsed = GetElapsedTime $itemtime
-  write-host "Elapsed Time: " $elapsed -foreground "red"
-}
-else
-{
-  $elapsed = GetElapsedTime $itemtime
-  write-host "Elapsed Time: " $elapsed -foreground "green"
-}
-
-If (Test-Path $LoadUCDataFile)
-{
-  write-host "$(get-date) Removing production LoadUCData file" -foreground "green"
-	Remove-Item $LoadUCDataFile -force
-}
-write-host "$(get-date) Copying LoadUCData.sql to production" -foreground "green"
-Copy-Item $LoadUCDataTempFile $LoadUCDataFile
 
 cd $ASNGenerated
 Convert-Path .
@@ -238,14 +100,6 @@ If (Test-Path $ResourcesGenerated)
   $elapsed = GetElapsedTime $itemtime
   write-host "Elapsed Time: " $elapsed -foreground "green"
 }
-
-cd $CSModelLocation
-Convert-Path .
-$itemtime = Get-Date
-write-host "$(get-date) Getting Latest Models" -foreground "green"
-& $TFSLocation get $CSModelArgs | Out-null
-$elapsed = GetElapsedTime $itemtime
-write-host "Elapsed Time: " $elapsed -foreground "green"
 
 $itemtime = Get-Date
 write-host "$(get-date) Importing Models" -foreground "green"
@@ -270,26 +124,33 @@ write-host "$(get-date) Generating UAR file" -foreground "green"
 & $ZipLocation a -tzip $URRGenerated $ResourcesGenerated\ | Out-null
 $elapsed = GetElapsedTime $itemtime
 write-host "Elapsed Time: " $elapsed -foreground "green"
-If (Test-Path "d:\messages\messagesgenerated.old")
+If (Test-Path $CDMessageOld)
 {
-  $itemtime = Get-Date
   write-host "$(get-date) Removing old UAR backup file" -foreground "green"
-	Remove-Item d:\messages\messagesgenerated.old -force
+	Remove-Item $CDMessageOld -force
+}
+write-host "$(get-date) Renaming messagesgenerated.uar to messagesgenerated.old" -foreground "green"
+Rename-Item $CDMessageNew messagesgenerated.old
+write-host "$(get-date) Copying new messagesgenerated.uar to TFS checkin directory" -foreground "green"
+Copy-Item $URRGenerated $CSMessageLocation
+$fileold = Get-Item $CDMessageOld
+$filenew = Get-Item $CDMessageNew
+
+if ($filenew.length -gt $fileold.length)
+{
+  cd $CSMessageLocation
+  Convert-Path .
+  $itemtime = Get-Date
+  write-host "$(get-date) Checking in messagesgenerated.uar" -foreground "green"
+  New-TfsChangeset -Item $CSMessageArgs -Verbose -Comment "Updated from DBUpdate script" -Override false
   $elapsed = GetElapsedTime $itemtime
   write-host "Elapsed Time: " $elapsed -foreground "green"
 }
-write-host "$(get-date) Renaming d:\messages\messagesgenerated.uar to messagesgenerated.old" -foreground "green"
-Rename-Item d:\messages\messagesgenerated.uar messagesgenerated.old
-write-host "$(get-date) Copying new messagesgenerated.uar to TFS checkin directory" -foreground "green"
-Copy-Item $URRGenerated $CSMessageLocation
-
-cd $CSMessageLocation
-Convert-Path .
-$itemtime = Get-Date
-write-host "$(get-date) Checking in messagesgenerated.uar" -foreground "green"
-New-TfsChangeset -Item $CSMessageArgs -Verbose -Comment "Updated from DBUpdate script" -Override false
-$elapsed = GetElapsedTime $itemtime
-write-host "Elapsed Time: " $elapsed -foreground "green"
+else
+{
+  write-host "$(get-date) File size problem, undoing pending changes to messagesgenerated.uar" -foreground "red"
+  & $TFSLocation undo $CSMessageArgs | Out-null
+}
 
 cd $PSScriptRoot
 Convert-Path .
