de6c525762e8d204e3dcb2f8c1988aa931402fe3
meprogrammergut <b65535@gmail.com>
2016-02-03 17:06:00 -0600

    clean up

--- ps/DBUpdate/DBUpdate.ps1
+++ ps/DBUpdate/DBUpdate.ps1
@@ -2,9 +2,6 @@
     Powershell DBUpdate script (Launches everything)
 #>
 $Host.UI.RawUI.WindowTitle = "DBUpdate Script - Launch everything"
-
 cmd /c start powershell -Command {.\RefreshCore.ps1}
-
-<#
 cmd /c start powershell -Command {.\GenerateUAR.ps1}
-#>
+
