8e928fe83b2937dc160fbe670c38d5cec5bf01fe
meprogrammerguy <b65535@gmail.com>
2016-02-06 14:12:34 -0600

    Working on simplifying XML settings

---
+++ ps/DBUpdate/testxml.ps1
@@ -0,0 +1,69 @@
+function GetTFSSource([string]$DriveSource) 
+{
+  $TFSSource = $DriveSource
+  $Args = $TFSSource -replace "\\", "/"
+  $Pieces = $Args.split("/")
+  $TFSSource  = "$/" + $Pieces[2] + "/" + $Pieces[3] + "/" + $Pieces[4] + "/" + $Pieces[5]
+  $TFSSource 
+}
+
+$CurrentUser = [Environment]::UserName
+[xml]$ConfigFile = Get-Content DBUpdatebetter.xml
+$CoreVersion = $ConfigFile.Settings.CoreVersion
+$Pieces = $CoreVersion.split(".")
+$MajorVersion = $Pieces[0]
+$MinorVersion = $Pieces[1]
+$OverrideDirectory = $ConfigFile.Settings.Users.$($CurrentUser).SettingsRoot + $MajorVersion + "_" + $MinorVersion + "\"
+if ($OverrideDirectory -ne "")
+{
+	$OverrideDirectory = $OverrideDirectory + "DBUpdatebetter.xml"
+	write-host "settings from $($OverrideDirectory)" -foreground "yellow"
+	[xml]$ConfigFile = Get-Content $OverrideDirectory
+}
+else
+{
+	write-host "settings from DBUpdate.xml" -foreground "yellow"
+}
+$CoreVersion = $ConfigFile.Settings.CoreVersion
+$Pieces = $CoreVersion.split(".")
+$MajorVersion = $Pieces[0]
+$MinorVersion = $Pieces[1]
+$TFSPath = $ConfigFile.Settings.HDriveRoot + "CS08." + $MajorVersion + "." + $MinorVersion + "\"
+$TFSIncludePath = $TFSPath + $ConfigFile.Settings.TFSIncludeFolder
+$TFSGlobalPath = $TFSPath + $ConfigFile.Settings.TFSGlobalFolder
+$TFSModelPath = $TFSPath + $ConfigFile.Settings.TFSModelFolder
+$TFSComponentPath = $TFSPath + $ConfigFile.Settings.TFSComponentFolder
+$IncludeArgs = GetTFSSource $TFSIncludePath
+$GlobalArgs = GetTFSSource $TFSGlobalPath
+$ModelArgs = GetTFSSource $TFSModelPath
+$ComponentArgs = GetTFSSource $TFSComponentPath
+$ImportIncludes = "XML:" + $TFSIncludePath + "\*." + $ConfigFile.Settings.TFSIncludeExtension
+$ImportGlobals = "XML:" + $TFSGlobalPath + "\*." + $ConfigFile.Settings.TFSGlobalExtension
+$ImportModels = "XML:" + $TFSModelPath + "\*." + $ConfigFile.Settings.TFSModelExtension
+$ImportComponent = "XML:" + $TFSComponentPath + "\*." + $ConfigFile.Settings.TFSComponentExtension
+$ASNCorePath = $ConfigFile.Settings.ASNCoreRoot + $MajorVersion + "_" + $MinorVersion + "\"
+$INICorePath = "/ini=" + $ASNCorePath + $ConfigFile.Settings.INICoreName
+$TempFileLocation = $ConfigFile.Settings.TempFileLocation
+$TFSWorkspace = $ConfigFile.Settings.GenerateUARFile.TFSWorkspaceRoot + $MajorVersion + "_" + $MinorVersion + "\"
+$MessageArgs = GetTFSSource $TFSPath
+$MessageArgs = $MessageArgs + $ConfigFile.Settings.GenerateUARFile.MessageArgs
+$SettingsRoot = 
+$MajorVersion
+$MinorVersion
+$TFSIncludePath
+$TFSGlobalPath 
+$TFSModelPath
+$TFSComponentPath
+$IncludeArgs
+$GlobalArgs
+$ModelArgs
+$ComponentArgs
+$ImportIncludes
+$ImportGlobals
+$ImportModels
+$ImportComponent
+$ASNCorePath
+$INICorePath
+$TempFileLocation
+$TFSWorkspace
+$MessageArgs
