74abde11a1daff3ffe21643cdffe2a284126018a
meprogrammerguy <b65535@gmail.com>
2017-03-21 13:41:22 -0500

    launch script

---
+++ ps/launch.ps1
@@ -0,0 +1,9 @@
+cd "C:\Program Files (x86)\Microsoft Office\root\Office16"
+Invoke-Item OUTLOOK.EXE
+cd "C:\Program Files (x86)\Mozilla Firefox"
+Invoke-Item firefox.exe
+cd "C:\Program Files\Internet Explorer"
+Invoke-Item iexplore.exe
+cd "C:\Program Files (x86)\Google\Chrome\Application"
+Invoke-Item chrome.exe
+cd "C:\Users\jsmith\Documents\git"
